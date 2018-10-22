5ffdf86e00ca46c69195b21067903de2e9f288f2
jae hnri <joao.henri.cr@gmail.com>
2018-10-22 11:57:59 -0300

    Da exceções e ModificarDono
    :+1:

--- SSP.sh
+++ SSP.sh
@@ -40,10 +40,15 @@ criarGrupo()
 	then
 		grupos
 	else
-	sudo groupadd $nomeGrupo
+	
+		if sudo groupadd $nomeGrupo
+		then
+			echo "Grupo chamado $nomeGrupo criado com sucesso!" 
+		else
+			echo "O programa deu erro!" 
+		fi
 	fi
 	echo
-	echo "Grupo chamado $nomeGrupo criado com sucesso!" 
 	echo
 	echo "Pressione qualquer botão para continuar!"
 	read
@@ -67,9 +72,13 @@ alterarGrupo()
 	echo
 	echo -n "Qual o novo nome? "
 	read nomeGrupoNovo
-	sudo groupmod -n $nomeGrupoNovo $nomeGrupoAntigo
 	echo
-	echo "Grupo chamado $nomeGrupoAntigo renomeado com sucesso! Seu novo nome é $nomeGrupoNovo" 
+	if sudo groupmod -n $nomeGrupoNovo $nomeGrupoAntigo
+	then	
+		echo "Grupo chamado $nomeGrupoAntigo renomeado com sucesso! Seu novo nome é $nomeGrupoNovo" 
+	else
+		echo "O programa deu erro!"
+	fi
 	echo
 	echo "Pressione qualquer botão para continuar!"
 	read
@@ -115,11 +124,15 @@ criarUsuario()
 	then
 		usuarios
 	else
-	sudo useradd $nomeUser
+	if sudo adduser $nomeUser
+	then
+		echo "Usuário chamado $nomeUser criado com sucesso!" 
+	else
+		echo "O programa deu erro!" 			
+	fi
 	fi
 	#considerar utilizar adduser
 	echo
-	echo "Usuário chamado $nomeUser criado com sucesso!" 
 	echo
 	echo "Pressione qualquer botão para continuar!"
 	read
@@ -154,13 +167,80 @@ permissoes()
 }
 
 modificarDono()
-{}
+{
+clear
+	echo "Projeto de Sistemas Operacionais por João Henri e Nicholas Patapoff"
+	echo "====================================================================="
+	echo
+	echo -n "Você está no seguinte diretório:"
+	pwd
+	echo
+	echo "Ele contem os seguintes arquivos e diretórios: "
+	echo
+	ls
+	echo	
+	echo
+	echo -n "Qual o nome da pasta ou diretório que você deseja mudar? "
+
+	read nomePastaDir
+
+	if [ -d $nomePastaDir ] && [ ! -z $nomePastaDir ]
+	then
+		echo -n "Quem será o novo dono? "
+		read novoDono
+		if sudo chown $novoDono $nomePastaDir
+		then
+			echo "Dono deste diretório foi alterado com sucesso!"
+			echo
+			echo
+			echo "Pressione qualquer botão para continuar!"
+			read
+			permissoes
+		else
+			echo "Dono inexistente!"; echo; echo; echo "Pressione qualquer botão para continuar!"; read; permissoes
+		fi
+	else
+		echo 
+	fi
+
+	if [ -f $nomePastaDir ] && [ ! -z $nomePastaDir ]
+	then
+		echo -n "Quem será o novo dono? "
+		read novoDono
+		if sudo chown $novoDono $nomePastaDir
+		then
+			echo "Dono deste arquivo foi alterado com sucesso!"
+			echo
+			echo
+			echo "Pressione qualquer botão para continuar!"
+			read
+			permissoes
+		else
+			echo "Dono inexistente!"; echo; echo; echo "Pressione qualquer botão para continuar!"; read; permissoes	
+		fi
+	else
+		echo
+	fi
+
+	echo "Diretório/Arquivo não encontrado!"
+	echo
+	echo
+	echo "Pressione qualquer botão para continuar!"
+	read
+	permissoes
+
+	
+}
 
 modificarGrupoDono()
-{}
+{
+echo
+}
 
 modificarPermissoes()
-{}
+{
+echo
+}
 
 
 # ////////////////////////////////////////////////////////////////////////////////MÉTODOS INICIAL///////////////////////////////////////////////////////////////
