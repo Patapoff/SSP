27f786fa6080620cb369099aa5c034f335f3bc9d
jae hnri <joao.henri.cr@gmail.com>
2018-10-23 11:21:28 -0300

    fim?

--- SSP.sh
+++ SSP.sh
@@ -166,6 +166,8 @@ permissoes()
 	esac
 }
 
+se
+
 modificarDono()
 {
 clear
@@ -234,12 +236,260 @@ clear
 
 modificarGrupoDono()
 {
-echo
+clear
+	echo "Projeto de Sistemas Operacionais por João Henri e Nicholas Patapoff"
+	echo "====================================================================="
+	echo
+	echo -n "Você está no seguinte diretório:"
+	pwd
+	echo
+	echo "Ele contém os seguintes arquivos e diretórios: "
+	echo
+	ls
+	echo	
+	echo
+	echo -n "Qual o nome da pasta ou diretório que você deseja mudar? "
+
+	read nomePastaDir
+
+	if [ -d $nomePastaDir ] && [ ! -z $nomePastaDir ]
+	then
+		echo -n "Quem será o novo grupo dono? "
+		read grupoDonoNovo
+		if sudo chgrp $grupoDonoNovo $nomePastaDir
+		then
+			echo "Grono deste diretório foi alterado com sucesso!"
+			echo
+			echo
+			echo "Pressione qualquer botão para continuar!"
+			read
+			permissoes
+		else
+			echo "Grupo dono inexistente!"; echo; echo; echo "Pressione qualquer botão para continuar!"; read; permissoes
+		fi
+	else
+		echo 
+	fi
+
+	if [ -f $nomePastaDir ] && [ ! -z $nomePastaDir ]
+	then
+		echo -n "Quem será o novo dono? "
+		read grupoDonoNovo
+		if sudo chown $grupoDonoNovo $nomePastaDir
+		then
+			echo "Grupo dono deste arquivo foi alterado com sucesso!"
+			echo
+			echo
+			echo "Pressione qualquer botão para continuar!"
+			read
+			permissoes
+		else
+			echo "Grupo dono inexistente!"; echo; echo; echo "Pressione qualquer botão para continuar!"; read; permissoes	
+		fi
+	else
+		echo
+	fi
+
+	echo "Diretório/Arquivo não encontrado!"
+	echo
+	echo
+	echo "Pressione qualquer botão para continuar!"
+	read
+	permissoes
 }
 
 modificarPermissoes()
 {
-echo
+clear
+	echo "Projeto de Sistemas Operacionais por João Henri e Nicholas Patapoff"
+	echo "====================================================================="
+	echo
+	echo -n "Você está no seguinte diretório:"
+	pwd
+	echo
+	echo "Ele contém os seguintes arquivos e diretórios: "
+	echo
+	ls
+	echo	
+	echo
+	echo -n "Qual o nome da pasta ou diretório que você deseja alterar as permissões? "
+
+	read nomePastaDir
+
+	if [ -d $nomePastaDir ] && [ ! -z $nomePastaDir ]
+	then		
+		clear
+		echo "Projeto de Sistemas Operacionais por João Henri e Nicholas Patapoff"
+		echo "====================================================================="
+		echo
+		echo "a) Modificar permissões do dono."
+		echo "b) Modificar permissões de grupo."
+		echo "c) Modificar permissões de outros."
+		echo "d) Voltar"
+		echo "e) Sair"
+		echo
+		echo -n "Qual a opção desejada? "
+	
+		read opcaoPermissoes
+		case $opcaoPermissoes in
+			a) permissoesDono ;;
+			b) permissoesGrupo ;;
+			c) permissoesOutros ;;
+			d) permissoes ;;
+			e) exit ;;
+			*) "Opção desconhecida." ; echo ; modificarPermissoes ;;
+		esac 
+	fi
+
+	if [ -f $nomePastaDir ] && [ ! -z $nomePastaDir ]
+	then
+		clear
+		echo "Projeto de Sistemas Operacionais por João Henri e Nicholas Patapoff"
+		echo "====================================================================="
+		echo
+		echo "a) Modificar permissões do dono."
+		echo "b) Modificar permissões de grupo."
+		echo "c) Modificar permissões de outros."
+		echo "d) Voltar"
+		echo "e) Sair"
+		echo
+		echo -n "Qual a opção desejada? "
+	
+		read opcaoPermissoes
+		case $opcaoPermissoes in
+			a) permissoesDono ;;
+			b) permissoesGrupo ;;
+			c) permissoesOutros ;;
+			d) permissoes ;;
+			e) exit ;;
+			*) "Opção desconhecida." ; echo ; modificarPermissoes ;;
+		esac 
+	fi	
+	fi
+
+	echo "Diretório/Arquivo não encontrado!"
+	echo
+	echo
+	echo "Pressione qualquer botão para continuar!"
+	read
+	modificarPermissoes
+}
+
+permissoesDono()
+{
+clear
+	echo "Projeto de Sistemas Operacionais por João Henri e Nicholas Patapoff"
+	echo "====================================================================="
+	echo
+	echo
+	echo "Quais permissões o dono possuirá?"
+	echo
+	echo "a) Nenhuma permissão."
+	echo "b) Execução."
+	echo "c) Escrita."
+	echo "d) Leitura."
+	echo "e) Escrita e execução."
+	echo "f) Leitura e execução."
+	echo "g) Leitura e escrita."
+	echo "h) Leitura e escrita e execução."
+	echo "i) Voltar."
+	echo "j) Sair."
+	echo
+	echo -n "Qual a opção desejada? "
+
+	read opcaoPermissoes
+	
+	case $opcaoPermissoes in
+		a) chmod u-rwx $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		b) chmod u+x $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		c) chmod u+w $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		d) chmod u+r $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		e) chmod u+wx $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		f) chmod u+rx $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		g) chmod u+rw $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		h) chmod u+rwx $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		i) modificarPermissoes ;;
+		j) exit ;;
+		*) "Opção desconhecida." ; echo ; permissoesDono ;;
+	esac
+}
+
+permissoesGrupo()
+{
+clear
+	echo "Projeto de Sistemas Operacionais por João Henri e Nicholas Patapoff"
+	echo "====================================================================="
+	echo
+	echo
+	echo "Quais permissões os grupos possuirão?"
+	echo
+	echo "a) Nenhuma permissão."
+	echo "b) Execução."
+	echo "c) Escrita."
+	echo "d) Leitura."
+	echo "e) Escrita e execução."
+	echo "f) Leitura e execução."
+	echo "g) Leitura e escrita."
+	echo "h) Leitura e escrita e execução."
+	echo "i) Voltar."
+	echo "j) Sair."
+	echo
+	echo -n "Qual a opção desejada? "
+
+	read opcaoPermissoes
+	
+	case $opcaoPermissoes in
+		a) chmod g-rwx $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		b) chmod g+x $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		c) chmod g+w $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		d) chmod g+r $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		e) chmod g+wx $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		f) chmod g+rx $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		g) chmod g+rw $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		h) chmod g+rwx $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		i) modificarPermissoes ;;
+		j) exit ;;
+		*) "Opção desconhecida." ; echo ; permissoesGrupo ;;
+	esac
+}
+
+permissoesOutros()
+{
+clear
+	echo "Projeto de Sistemas Operacionais por João Henri e Nicholas Patapoff"
+	echo "====================================================================="
+	echo
+	echo
+	echo "Quais permissões os outros possuirão?"
+	echo
+	echo "a) Nenhuma permissão."
+	echo "b) Execução."
+	echo "c) Escrita."
+	echo "d) Leitura."
+	echo "e) Escrita e execução."
+	echo "f) Leitura e execução."
+	echo "g) Leitura e escrita."
+	echo "h) Leitura e escrita e execução."
+	echo "i) Voltar."
+	echo "j) Sair."
+	echo
+	echo -n "Qual a opção desejada? "
+
+	read opcaoPermissoes
+	
+	case $opcaoPermissoes in
+		a) chmod o-rwx $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		b) chmod o+x $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		c) chmod o+w $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		d) chmod o+r $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		e) chmod o+wx $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		f) chmod o+rx $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		g) chmod o+rw $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		h) chmod o+rwx $nomePastaDir ; echo "Permissões alteradas com sucesso!" ; echo ; echo ; echo "Pressione qualquer botão para continuar!" ; echo ; read ; permissoes ;;
+		i) modificarPermissoes ;;
+		j) exit ;;
+		*) "Opção desconhecida." ; echo ; permissoesOutros ;;
+	esac
 }
 
 
